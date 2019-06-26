e52d8e399b5f4153df741468b6e712e3663fced0
kutaybuyukkorukcu <kutaybuyukkorukcu@gmail.com>
2019-06-26 21:30:10 +0300

    Pushed some of the react code

---
+++ ReactJS/PassingCallbacks.js
@@ -0,0 +1,24 @@
+class MyApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      inputValue: event.target.value
+    });
+  }
+  render() {
+    return (
+       <div>
+        <GetInput input={this.state.inputValue} handleChange={this.handleChange}/>
+        }
+        <RenderInput input={this.state.inputValue}/>
+        }
+       </div>
+    );
+  }
+};
--- python-oop/oopex-1.py
+++ python-oop/oopex-1.py
@@ -1,37 +1,43 @@
 class Employee():
-  def __init__(self, name, salary, department):
-    self.name = name
-    self.salary = salary
-    self.department = department
-  
-  def display_results(self):
-    print("Displaying the information of the working class")
-    print("Name:", self.name, "Salary:", self.salary, "Department:", self.department)
-  
-  def search_salary(self, raise_amount):
-    print("salary is raised")
-    self.salary += raise_amount
+    def __init__(self, name, salary, department):
+        self.name = name
+        self.salary = salary
+        self.department = department
 
-  def change_department(self, new_department):
-    print("Changing department...")
-    self.department = new_department
+    def display_results(self):
+        print("Displaying the information of the working class")
+        print("Name:", self.name, "Salary:", self.salary,
+              "Department:", self.department)
+
+    def search_salary(self, raise_amount):
+        print("salary is raised")
+        self.salary += raise_amount
+
+    def change_department(self, new_department):
+        print("Changing department...")
+        self.department = new_department
 
 # class Manager(Employee, someKindOfGodOfWar)
+
+
 class Manager(Employee):
-  def __init__(self, name, salary, department, number_of_worker):
-    super().__init__(name, salary, department)
-    self.number_of_worker = number_of_worker
-  
-  def display_results(self):
-    print("Displaying the information of manager class")
-    print("Name:", self.name, "Salary:", self.salary, "Department:", self.department, "Number of Workers", self.number_of_worker)
+    def __init__(self, name, salary, department, number_of_worker):
+        super().__init__(name, salary, department)
+        self.number_of_worker = number_of_worker
 
-  def increase_number_of_worker(self, inc):
-    print("Increasing number of worker")
-    self.number_of_worker += inc
+        def display_results(self):
+            print("Displaying the information of manager class")
+            print("Name:", self.name, "Salary:", self.salary, "Department:",
+            self.department, "Number of Workers", self.number_of_worker)
 
-manager = Manager("Kutay Buyukkorukcu", 0, "Broke Student :)", 20)
+        def increase_number_of_worker(self, inc):
+            print("Increasing number of worker")
+            self.number_of_worker += inc
 
-manager.display_results()
+        manager = Manager("Kutay Buyukkorukcu", 0, "Broke Student :)", 20)
+
+    manager.display_results()
+
+
 manager.increase_number_of_worker(10)
 manager.display_results()
